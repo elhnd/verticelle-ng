4c8131e5afca28a8da59da21db2ffe6114c154bc
elhadji <eldji22@hotmail.fr>
2024-02-28 17:18:44 +0100

    refacto config

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,42 +1,20 @@
-import { inject } from '@angular/core';
-import { HttpInterceptorFn } from '@angular/common/http';
-import { tap } from 'rxjs';
-import { AuthService } from '@core/services/auth.service';
-import { Router } from '@angular/router';
-import { TokenService } from '@core/services/token.service';
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
+import { TokenService } from "@core/services/token.service";
 
-const TOKEN_HEADER_KEY = 'Authorization';
-//Ne pas enlever l'espace sur le string 
-const BEARER = 'Bearer ';
+export const authInterceptor : HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) =>{
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  
-  const _authService    = inject(AuthService);
-  const _tokenService   = inject(TokenService);
-  const _router         = inject(Router);
+    const tokenService = inject(TokenService);
+    
+    if(!tokenService.isTokenExpired()){
+        inject(Router).navigateByUrl("auth/login");
+        return next(req);
+    }
 
-  return next(req).pipe(
-    tap({
-      next: () => {
-        let authReq = req;
-
-        const token = _tokenService.getToken();
-
-        _authService.isAuthenticated$.subscribe((auth) => {
-            if(auth) {
-                authReq = req.clone({
-                    headers: req.headers.set(TOKEN_HEADER_KEY, BEARER + token),
-                });
-            } else {
-                _router.navigateByUrl("auth/login");
-            }
-        });
-
-        return authReq;
-      }
-      // ,
-      // error: () => {},
-      // complete: () => {},
-    })
-  );
-};
+    const reqWithHeader = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${tokenService.getToken()}`),
+    });
+        
+    return next(reqWithHeader);  
+}
